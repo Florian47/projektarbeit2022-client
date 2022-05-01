f8095e0192bae750562161c5dd384eee1e9b3ded
chris <chris.brinkhofg@gmail.com>
2022-05-01 20:48:50 +0200

    Dozent Added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,13 +6,14 @@ import { AuthGuard } from './_helpers';
 
 const accountModule = () => import('./account/account.module').then(x => x.AccountModule);
 const usersModule = () => import('./users/users.module').then(x => x.UsersModule);
-const schuelerModule = () => import('./schueler/schueler.module').then(x => x.SchuelerModule);
-
+const schuelerModule = () => import('./_schuelerHomeScreen/schueler.module').then(x => x.SchuelerModule);
+const dozentModule = () => import('./_dozentHomeScreen/dozent.module').then(x => x.DozentModule);
 
 const routes: Routes = [
   { path: '', component: HomeComponent, canActivate: [AuthGuard] },
   { path: 'users', loadChildren: usersModule, canActivate: [AuthGuard] },
   { path: 'schueler', loadChildren: schuelerModule, canActivate: [AuthGuard] },
+  { path: 'dozent', loadChildren: dozentModule, canActivate: [AuthGuard] },
   { path: 'account', loadChildren: accountModule },
 
   // otherwise redirect to home
