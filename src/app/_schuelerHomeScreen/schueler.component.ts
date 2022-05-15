1869d4765cba87144d1618e89b3ea57abbbadbba
chris <chris.brinkhofg@gmail.com>
2022-05-15 23:14:38 +0200

    warten Auf Entity

--- src/app/_schuelerHomeScreen/schueler.component.ts
+++ src/app/_schuelerHomeScreen/schueler.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {TaskDifficulty} from "../_models/task.difficulty";
+import {TaskCategory} from "../_models/task.category";
+
 
 @Component({
   selector: 'app-html',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SchuelerComponent implements OnInit {
   trainingsliste: string[];
+  // @ts-ignore
+  model: task = new task();
+  difficultyOptions: TaskDifficulty[];
+  taskTypeOptions: TaskCategory[];
+
 
   constructor() {
     this.trainingsliste=['Training Brinkhoff Grammatik','Training Brinkhoff groß/klein Schreibung']
+    this.difficultyOptions = [TaskDifficulty.EASY, TaskDifficulty.MEDIUM, TaskDifficulty.HARD];
+    this.taskTypeOptions = [TaskCategory.GRAMMATIK, TaskCategory.LUECKENTEXT, TaskCategory.ZEICHENSETZUNG, TaskCategory.GROSS_KLEIN_SCHREIBUNG];
   }
 
   ngOnInit(): void {
