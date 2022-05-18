170edd0ac705c0ce524bea8b7e40699c83f18363
Florian <weinertflorian@googlemail.com>
2022-05-18 15:09:09 +0200

    read user id from service

--- src/app/_schuelerHomeScreen/schueler.component.ts
+++ src/app/_schuelerHomeScreen/schueler.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {TaskDifficulty} from "../_models/task.difficulty";
 import {TaskCategory} from "../_models/task.category";
 import {StudentGeneratedTraining} from "../_models/student.generated.training";
@@ -7,8 +7,6 @@ import {Training} from "../_models/training";
 import {first} from "rxjs/operators";
 import {Router} from "@angular/router";
 import {AccountService} from "../_services";
-import {User} from "../_models";
-import {BehaviorSubject, Observable} from "rxjs";
 
 
 @Component({
@@ -17,45 +15,39 @@ import {BehaviorSubject, Observable} from "rxjs";
   styleUrls: ['./schueler.component.css']
 })
 export class SchuelerComponent implements OnInit {
-  trainingsliste: Training[]=[];
+  trainingsliste: Training[] = [];
   difficultyOptions: TaskDifficulty[];
   taskTypeOptions: TaskCategory[];
-  model: StudentGeneratedTraining= new StudentGeneratedTraining();
-  private userSubject: BehaviorSubject<User | null>;
-  public user: Observable<User>;
+  model: StudentGeneratedTraining = new StudentGeneratedTraining();
 
 
-  constructor(private accountService:AccountService, private trainingsService:TrainingService, private router : Router) {
-    this.userSubject = new BehaviorSubject<User | null>(JSON.parse(<string>localStorage.getItem('user')));
-    this.user = <Observable<User>> this.userSubject.asObservable();
-
-
-
-    trainingsService.getAllTrainingsForStudent(this.userValue.id).subscribe((training:Training[]) =>{this.trainingsliste= training} )
+  constructor(private accountService: AccountService, private trainingsService: TrainingService, private router: Router) {
+    trainingsService.getAllTrainingsForStudent(accountService.userValue.id).subscribe((training: Training[]) => {
+      this.trainingsliste = training
+    })
     this.difficultyOptions = [TaskDifficulty.EASY, TaskDifficulty.MEDIUM, TaskDifficulty.HARD];
     this.taskTypeOptions = [TaskCategory.GRAMMATIK, TaskCategory.LUECKENTEXT, TaskCategory.ZEICHENSETZUNG, TaskCategory.GROSS_KLEIN_SCHREIBUNG];
 
   }
-  public get userValue(): User {
-    return <User>this.userSubject.value;
-  }
 
   ngOnInit(): void {
   }
-  onSubmit(){
+
+  onSubmit() {
 
   }
 
 
-  createTraining(){
+  createTraining() {
     this.trainingsService.createStudentTraining(this.model).pipe(first()).subscribe((training: Training) => {
-      this.router.navigate(['/doTraining/'+training.id]);
+      this.router.navigate(['/doTraining/' + training.id]);
     });
 
-   }
-   startTraining(id:number){
+  }
 
-       this.router.navigate(['/doTraining/'+id]);
+  startTraining(id: number) {
+
+    this.router.navigate(['/doTraining/' + id]);
 
   }
 
