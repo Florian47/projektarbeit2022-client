2235e8183d93421de45ebebf90972518dfd12310
Florian <weinertflorian@googlemail.com>
2022-06-02 16:28:20 +0200

    rebase

--- src/app/_models/solution.options.ts
+++ src/app/_models/solution.options.ts
@@ -13,7 +13,7 @@ export class SolutionOptions {
   constructor(public id: number=0,
               public optionName: string='',
               public notUniqueId: string='',
-              public rightAnswer: boolean=false,
+              public checkedAnswer: boolean=false,
   ) {
   }
 }
